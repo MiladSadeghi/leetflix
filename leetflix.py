5e5b7f60bb1ba154a41d02879702f718d9ff4a1d
milad <miladsadeghi2323@gmail.com>
2021-06-09 18:07:11 +0430

    change text area to treeview :|

--- leetflix.py
+++ leetflix.py
@@ -1,8 +1,14 @@
-import json 
+import json
+from tkinter.messagebox import *
 import requests
 import subprocess
 from tkinter import *
+from tkinter import ttk
 
+#api
+API_URL = "https://leetflix.haghiri75.com"
+
+#position app
 def geo(window,x,y):
     scr_width, scr_height = window.winfo_screenwidth(), window.winfo_screenheight()
     scr_x, scr_y = (scr_width/2) - (x/2), (scr_height/2) - (y/2)
@@ -26,12 +32,14 @@ movieName_label.grid(sticky=W, pady=(0,10))
 movieName_entry.grid(pady=(0,10), ipady=2)
 
 #button Find!
-findMovie_button = Button(rootFrame ,text='Find', width=38)
+findMovie_button = Button(rootFrame ,text='Find', width=38, command=findCommand)
 findMovie_button.grid(pady=(0,14))
 
-#text area for movie list
-movielist_text = Text(rootFrame, width=34, height=14)
-movielist_text.grid()
+#table for movie list
+movielist_table = ttk.Treeview(rootFrame, show='headings', columns=(0))
+movielist_table.grid()
+movielist_table.column(0, width=275, stretch=NO, anchor="center")
+movielist_table.heading(0, text='Movies', anchor=W)
 
 #download button
 downloadButton = Button(rootFrame, text='Download', width=38)
