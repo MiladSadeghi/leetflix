522f04c5238b657cd5d7ec24db499364bef61a68
milad <miladsadeghi2323@gmail.com>
2021-06-09 16:18:53 +0430

    button find complte

--- leetflix.py
+++ leetflix.py
@@ -6,7 +6,7 @@ from tkinter import *
 def geo(window,x,y):
     scr_width, scr_height = window.winfo_screenwidth(), window.winfo_screenheight()
     scr_x, scr_y = (scr_width/2) - (x/2), (scr_height/2) - (y/2)
-    x = '%dx%d+%d+%d' % (x, y, scr_x, scr_y)
+    x = "%dx%d+%d+%d" % (x, y, scr_x, scr_y)
     return x
 
 #window
@@ -15,10 +15,18 @@ win.title('LEETFLIX')
 win.geometry(geo(win, 350, 400))
 win.resizable(FALSE, FALSE)
 
+# """highlightbackground="black", highlightthickness=1"""
+test = Frame(win)
+test.grid(padx=37, pady=(20,0))
+
 #enter movie name
-movieName_label = Label(win, text='Movie name (or Keywords)', font=('',9))
-movieName_entry = Entry(win, width=30, font=('',13), justify=CENTER)
-movieName_label.place(x = 30, y = 20)
-movieName_entry.place(x = 33, y = 47)
+movieName_label = Label(test, text='Movie name (or Keywords)', font=('',9))
+movieName_entry = Entry(test, width=30, font=('',13), justify=CENTER)
+movieName_label.grid(sticky=W, pady=(0,10))
+movieName_entry.grid(pady=(0,10), ipady=2)
+
+#button Find!
+findMovie_button = Button(test ,text='Find', width=38)
+findMovie_button.grid()
 
 win.mainloop()
