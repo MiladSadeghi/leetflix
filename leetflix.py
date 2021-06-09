b1057519497f42b43bbe1a79064100528cdf03f7
milad <miladsadeghi2323@gmail.com>
2021-06-09 12:05:20 +0430

    Final Change!

--- leetflix.py
+++ leetflix.py
@@ -4,24 +4,22 @@ import subprocess
 
 API_URL = "https://leetflix.haghiri75.com"
 
-def program(movie_name):
+def program():
+    movie_name = input("Enter a movie name (or keywords): ")
     response = requests.post(API_URL, data=json.dumps({"keyword" : movie_name}), headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100101 Firefox/88.0', 'content-type': 'application/json'})
     response = json.loads(response.text)
     if len(response) == 0:
-        return False
+        return print('Nothing found!')
     index = 1
     for result in response:
         print(f'{index} : {result["title"]}')
         index += 1
     choice = int(input("Enter an index: "))
+    if choice > 15:
+        return print("Not in list!")
     magnet_link = response[choice - 1]['magnet']
-    execution_array = ['webtorrent', magnet_link, '--vlc']
+
+    execution_array = """webtorrent "{}" --vlc""".format(magnet_link)
     subprocess.run(execution_array, shell=True)
 
-j = 0
-while j == 0 :
-    movie_name = input("Enter a movie name (or keywords): ")
-    if program(movie_name):
-        pass
-    else:
-        print('Nothing Found!')
+program()
