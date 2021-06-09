b4bc29ddb0200e02628dad96cc12cf41eab5f8b7
milad <miladsadeghi2323@gmail.com>
2021-06-09 17:17:42 +0430

    download button complete

--- leetflix.py
+++ leetflix.py
@@ -15,22 +15,26 @@ win.title('LEETFLIX')
 win.geometry(geo(win, 350, 400))
 win.resizable(FALSE, FALSE)
 
-# """highlightbackground="black", highlightthickness=1"""
-test = Frame(win)
-test.grid(padx=37, pady=(20,0))
+
+rootFrame = Frame(win)
+rootFrame.grid(padx=37, pady=(13,0))
 
 #enter movie name
-movieName_label = Label(test, text='Movie name (or Keywords)', font=('',9))
-movieName_entry = Entry(test, width=30, font=('',13), justify=CENTER)
+movieName_label = Label(rootFrame, text='Movie name (or Keywords)', font=('',9))
+movieName_entry = Entry(rootFrame, width=30, font=('',13), justify=CENTER)
 movieName_label.grid(sticky=W, pady=(0,10))
 movieName_entry.grid(pady=(0,10), ipady=2)
 
 #button Find!
-findMovie_button = Button(test ,text='Find', width=38)
+findMovie_button = Button(rootFrame ,text='Find', width=38)
 findMovie_button.grid(pady=(0,14))
 
 #text area for movie list
-movielist_text = Text(test, width=34, height=13)
+movielist_text = Text(rootFrame, width=34, height=14)
 movielist_text.grid()
+
+#download button
+downloadButton = Button(rootFrame, text='Download', width=38)
+downloadButton.grid(pady=(10,0))
 
 win.mainloop()
