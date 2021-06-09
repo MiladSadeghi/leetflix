1e89484b2f6b32fcf28b49fbe7143bec2f034bfd
milad <miladsadeghi2323@gmail.com>
2021-06-09 14:04:01 +0430

    create window

--- leetflix.py
+++ leetflix.py
@@ -1,26 +1,18 @@
 import json 
 import requests
 import subprocess
+from tkinter import *
 
-API_URL = "https://leetflix.haghiri75.com"
+def geo(window,x,y):
+    scr_width, scr_height = window.winfo_screenwidth(), window.winfo_screenheight()
+    scr_x, scr_y = (scr_width/2) - (x/2), (scr_height/2) - (y/2)
+    x = '%dx%d+%d+%d' % (x, y, scr_x, scr_y)
+    return x
 
-def program():
-    movie_name = input("Enter a movie name (or keywords): ")
-    response = requests.post(API_URL, data=json.dumps({"keyword" : movie_name}), headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100101 Firefox/88.0', 'content-type': 'application/json'})
-    response = json.loads(response.text)
-    if len(response) == 0:
-        return print('Nothing found!')
-    index = 1
-    for result in response:
-        print(f'{index} : {result["title"]}')
-        index += 1
-    choice = int(input("Enter an index: "))
-    if choice > 15:
-        return print("Not in list!")
-    magnet_link = response[choice - 1]['magnet']
+win = Tk()
+win.title('LEETFLIX')
+win.geometry(geo(win, 400, 400))
+win.resizable(FALSE, FALSE)
 
-    execution_array = """webtorrent "{}" --vlc""".format(magnet_link)
-    subprocess.run(execution_array, shell=True)
 
-while 1:
-    program()
+win.mainloop()
